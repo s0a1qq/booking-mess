78cea073576c15816863da28e80cd03ebaefa1ce
MikeKras <35962207+s0a1qq@users.noreply.github.com>
2022-02-11 01:18:33 +0300

    render first test

---
+++ internal/render/render_test.go
@@ -0,0 +1,43 @@
+package render
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/justinas/nosurf"
+	"github.com/s0a1qq/booking-mess/internal/models"
+)
+
+func TestAddDefaultData(t *testing.T) {
+	var td models.TemplateData
+
+	r, err := getSession()
+	if err != nil {
+		t.Error(err)
+	}
+
+	session.Put(r.Context(), "flash", "123")
+	result := AddDefaultData(&td, r)
+
+	if result.Flash != "123" {
+		t.Error("flash value is not 123")
+	}
+
+	td.Flash = app.Session.PopString(r.Context(), "flash")
+	td.Error = app.Session.PopString(r.Context(), "error")
+	td.Warning = app.Session.PopString(r.Context(), "warning")
+	td.CSRFToken = nosurf.Token(r)
+}
+
+func getSession() (*http.Request, error) {
+	r, err := http.NewRequest("GET", "/url", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := r.Context()
+	ctx, _ = session.Load(ctx, r.Header.Get("X-Session"))
+	r = r.WithContext(ctx)
+
+	return r, nil
+}
